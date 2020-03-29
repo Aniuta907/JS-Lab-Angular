879dbd207ac4dc6291f41ced6fc1b720df5becc6
Anna Kuznetsova <Aniuta907email@gmail.com>
2020-03-29 21:54:25 +0300

    hw2 switch view and catch/release were added

--- Pokedex/src/app/pokemon/pokemon-card/pokemon-card.component.ts
+++ Pokedex/src/app/pokemon/pokemon-card/pokemon-card.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Pokemon } from "../pokemon-list/pokemon-list.component";
+import { Component, OnInit, Input } from '@angular/core';
+import { Pokemon } from '../pokemon-list/pokemon-list.component';
 
 @Component({
-  selector: "app-pokemon-card",
-  templateUrl: "./pokemon-card.component.html",
-  styleUrls: ["./pokemon-card.component.scss"]
+	selector: 'app-pokemon-card',
+	templateUrl: './pokemon-card.component.html',
+	styleUrls: [ './pokemon-card.component.scss' ]
 })
 export class PokemonCardComponent implements OnInit {
-  @Input() pokemon: Pokemon;
-  caught = false;
+	@Input() pokemon: Pokemon;
+	caught = false;
 
-  constructor() {}
+	constructor() {}
 
-  ngOnInit(): void {}
+	ngOnInit(): void {}
 
-  catchPokemon() {
-    this.caught = !this.caught;
-    if (this.caught) {
-      console.log('Pokemon ' + this.pokemon.name + ' is caught');
-    } else {
-      console.log('Pokemon ' + this.pokemon.name + ' is free');
-    }
-  }
+	catchPokemon() {
+		this.caught = !this.caught;
+		if (this.caught) {
+			console.log(this.pokemon.name + ' is caught');
+		} else {
+			console.log(this.pokemon.name + ' is free');
+		}
+	}
 }
