9524975075516fc346b6c6c467b340ffbb0fa27e
Anna Kuznetsova <Aniuta907email@gmail.com>
2020-04-25 20:37:49 +0300

    Added switching between pages

--- Pokedex/src/app/app-routing.module.ts
+++ Pokedex/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.compon
 import { PokemonPageComponent } from './pokemon/pokemon-page/pokemon-page.component';
 
 const routes: Routes = [
-  { path: '', component: PokemonListComponent },
+  { path: 'home', component: PokemonListComponent },
+  { path: 'caught', component: PokemonPageComponent },
   { path: 'pokemons/:pokemonId', component: PokemonPageComponent },
   { path: '**', redirectTo: '/'}
 ];
--- Pokedex/src/app/app.module.ts
+++ Pokedex/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+
+const appRoutes: Routes = [
+  { path: 'main', component: PokemonListComponent }
+];
 
 @NgModule({
   declarations: [
